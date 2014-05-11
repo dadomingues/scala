f8a542977b1ec131f602a6d67fef914bdb9f244f
Daniel Araujo Domingues <dadomingues@users.noreply.github.com>
2014-05-11 16:32:28 -0300

    Update week1.scala
    adição de comentários no código

--- week1.scala
+++ week1.scala
@@ -15,6 +15,9 @@ object Main {
    * Exercicio 1
    */
   def pascal(c: Int, r: Int): Int = {
+    // recebe a referência de coluna e linha
+    // e retorna qual o valor que esta posição
+    // tem no triângulo de Pascal.
     if (c == 0 || r == c || r == 0) 1
     else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
@@ -23,11 +26,13 @@ object Main {
    * Exercicio 2
    */
   def balance(chars: List[Char]): Boolean = {
-    def iterate(chars: List[Char], qtOpen: Int): Int = {
-      if (chars.isEmpty || qtOpen < 0) qtOpen
-      else if (chars.head == '(') iterate(chars.tail, qtOpen + 1)
-      else if (chars.head == ')') iterate(chars.tail, qtOpen - 1)
-      else iterate(chars.tail, qtOpen)
+    // recebe uma sequencia de caracteres para verificar
+    // se os parênteses foram abertos e fechados corretamente
+    def iterate(chars: List[Char], qtOpened: Int): Int = {
+      if (chars.isEmpty || qtOpened < 0) qtOpened
+      else if (chars.head == '(') iterate(chars.tail, qtOpened + 1)
+      else if (chars.head == ')') iterate(chars.tail, qtOpened - 1)
+      else iterate(chars.tail, qtOpened)
     }
     iterate(chars, 0) == 0
   }
@@ -36,6 +41,8 @@ object Main {
    * Exercicio 3
    */
   def countChange(money: Int, coins: List[Int]): Int = {
+    // dado valor em dinheiro, calcula o troco
+    // conforme moedas disponíveis
     if (money == 0) 1
     else if (money < 0 || coins.isEmpty) 0
     else if (money <= 0 && !coins.isEmpty) 0
